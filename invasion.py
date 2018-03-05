76c187d21537b09c8c90c7e510b326ef77d5d096
Patrick Werner <kug3lblitz@protonmail.com>
2018-03-05 16:49:11 -0500

    bullets are deleted after leaving the screen, maximum number of bullets limited to 3

--- invasion.py
+++ invasion.py
@@ -21,6 +21,13 @@ def run_game():
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        # remove bullets that have passed offscreen
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        # print(len(bullets))
+
         gf.update_screen(game_settings, screen, ship, bullets)
 
 run_game()
